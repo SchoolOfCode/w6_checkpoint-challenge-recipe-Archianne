88d9476e0c0f608cdad9f76569fa5cd8b1a26557
Archianne <helena19w@gmail.com>
2021-05-05 17:26:38 +0100

    added event listener

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ let foodToSearch = null;
 //important
 const APP_ID = 'acf6c552';
 const APP_key = '382791db90157c92aaabdea59f48f996';
+
+//addEventListener
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  foodToSearch = e.target.querySelector("input").value;
+  fetchRecipe();
+});
