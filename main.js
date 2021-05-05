1beb9e274f2e79c64d501197f834901ce1f76663
Archianne <helena19w@gmail.com>
2021-05-05 18:01:32 +0100

    added food results to html

--- main.js
+++ main.js
@@ -22,6 +22,37 @@ async function fetchRecipe() {
   const baseURL = `https://api.edamam.com/search?q=${foodToSearch}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
   const response = await fetch(baseURL);
   const data = await response.json();
-  handleFoodChange(data.hits);
+  handleFood(data.hits);
 }
 
+function handleFood(results) {
+  container.classList.remove("initial");
+  let foodList = "";
+  results.map((foodResult) => {
+    foodList += `
+    
+    <div class="item">
+      <img src="${foodResult.recipe.image}" alt="img">
+    <div class="flex-container">
+    <h2 class="title">${foodResult.recipe.label}</h2>
+    <a class="view-btn" target="_blank" href="${foodResult.recipe.url}">View Recipe</a>
+    <p class="item-data">Number of servings: ${
+      foodResult.recipe.yield
+    }
+    </p>
+    </div>
+    <p class="item-data">Calories: ${
+      foodResult.recipe.calories.toFixed(2)
+    }
+    </p>
+    <p class="item-data">Diet label: ${
+      foodResult.recipe.dietLabels.length > 0 ? foodResult.recipe.dietLabels : "No Data Found"
+    } 
+    </p>
+    </div>
+    `;
+  });
+
+  //add result to html
+  searchResult.innerHTML = foodList;
+}
