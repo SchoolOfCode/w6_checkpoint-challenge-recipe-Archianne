cfef94d90915c937bdda89c087920f4c21001a5a
Archianne <helena19w@gmail.com>
2021-05-05 17:27:27 +0100

    async function

--- main.js
+++ main.js
@@ -13,3 +13,12 @@ searchForm.addEventListener("submit", (e) => {
   foodToSearch = e.target.querySelector("input").value;
   fetchRecipe();
 });
+
+//async function
+async function fetchRecipe() {
+  const baseURL = `https://api.edamam.com/search?q=${foodToSearch}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  generateHTML(data.hits);
+  console.log(data);
+}
