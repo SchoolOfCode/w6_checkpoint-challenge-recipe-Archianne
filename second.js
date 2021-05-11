7f6a169f9e43e1c88268c2f3c67e4233f297e4c5
Archianne <helena19w@gmail.com>
2021-05-11 23:25:23 +0100

    adding a third?

--- second.js
+++ second.js
@@ -1,10 +1,22 @@
 const backgroundCover = document.querySelector(".background-cover");
+const cocktailResult = document.querySelector(".cocktail-result");
 
 async function getRandomPicture() {
   const url = "https://foodish-api.herokuapp.com/api";
   const response = await fetch(url);
-  const data = await response.json();
-  backgroundCover.style.backgroundImage = `url(${data.image})`;
+  const picData = await response.json();
+  backgroundCover.style.backgroundImage = `url(${picData.image})`;
 }
 getRandomPicture();
 
+async function randomCocktail() {
+  const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+  const response = await fetch(url);
+  const cocktailData = await response.json();
+  console.log(cocktailData.drinks[0].strDrink);
+  const cocktail = cocktailData.drinks[0];
+  
+  cocktailResult.innerHTML = `<p>${cocktail.strDrink}</p>`;
+}
+
+randomCocktail();
