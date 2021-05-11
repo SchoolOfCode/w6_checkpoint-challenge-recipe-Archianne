5db2b99f3b58875feb9659bcdd63c7769bf7aead
Archianne <helena19w@gmail.com>
2021-05-11 17:51:58 +0100

    add eventListener to enter

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const searchResult = document.querySelector(".search-result");
 const container = document.querySelector(".container");
+const button = document.querySelector("#recipe-button");
+const input = document.querySelector("#food-input");
 let foodToSearch = null;
 //important
 const APP_ID = "c1d6ad2a";
@@ -10,13 +12,21 @@ function handleRecipeClick() {
 }
 
 function handleFoodChange() {
-  foodToSearch = document.querySelector("#food-input").value;
+  foodToSearch = input.value;
+  input.value = "";
 }
+
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    button.click();
+  }
+});
 
 //async function
 async function fetchRecipe(food) {
   foodToSearch = food;
-  const requestUrl = `https://api.edamam.com/search?q=${foodToSearch}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=18`;
+  const requestUrl = `https://api.edamam.com/search?q=${foodToSearch}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=3`;
   const response = await fetch(requestUrl);
   const data = await response.json();
   handleFood(data.hits);
@@ -24,24 +34,25 @@ async function fetchRecipe(food) {
 
 function handleFood(results) {
   results.map((foodToSearch) => {
+    let result = foodToSearch.recipe;
     searchResult.innerHTML += `
     <div class="items">
-      <a target="_blank" href="${foodToSearch.recipe.url}"><img src="${
-      foodToSearch.recipe.image
-      }" alt="${foodToSearch.recipe.label}"></a>
+      <a target="_blank" href="${result.url}"><img src="${
+        result.image
+      }" alt="${result.label}"></a>
     <div class="resultsDisplay">
-      <h2 class="title">${foodToSearch.recipe.label}</h2>
+      <h2 class="title">${result.label}</h2>
       <a class="viewLink" target="_blank" href="${
-      foodToSearch.recipe.url
+        result.url
       }">View Recipe</a>
     
     </div>
-      <p class="itemInfo">Number of servings: ${foodToSearch.recipe.yield}</p>
-      <p class="itemInfo">${foodToSearch.recipe.cuisineType}</p>
-      <p class="itemInfo">Calories: ${foodToSearch.recipe.calories.toFixed(0)}</p>
+      <p class="itemInfo">Number of servings: ${result.yield}</p>
+      <p class="itemInfo">${result.cuisineType}</p>
+      <p class="itemInfo">Calories: ${result.calories.toFixed(0)}</p>
       <p class="itemInfo">Diet label: ${
-      foodToSearch.recipe.dietLabels.length > 0
-        ? foodToSearch.recipe.dietLabels
+        result.dietLabels.length > 0
+        ? result.dietLabels
         : "Not found"
     } </p>
     </div>
