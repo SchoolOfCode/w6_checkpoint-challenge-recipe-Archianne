481188c4c6be47516b4732ce753dd6fdbc79ebd9
Archianne <helena19w@gmail.com>
2021-05-11 22:21:17 +0100

    second api succefully added

---
+++ second.js
@@ -0,0 +1,9 @@
+const backgroundCover = document.querySelector(".background-cover");
+
+async function getRandomPicture() {
+  const url = "https://foodish-api.herokuapp.com/api";
+  const response = await fetch(url);
+  const data = await response.json();
+  backgroundCover.style.backgroundImage = `url(${data.image})`;
+}
+getRandomPicture();
